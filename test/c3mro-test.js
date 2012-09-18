0c144adeb7553783f8ac1fc95340ddbe12498a1a
Marenin Alex <great_muchacho@mail.ru>
2012-09-18 12:35:44 +0400

    Added test page for c3mro

---
+++ test/c3mro-test.js
@@ -0,0 +1,11 @@
+/**
+ * @author Marenin Alex
+ * September 2012
+ */
+
+module( 'C3 Linearization(method resolution order) test' );
+
+test( 'Check for existence', function(){
+	test( typeof c3mro === 'function', 'check for existence of c3mro function' );
+
+});
